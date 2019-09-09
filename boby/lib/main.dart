25fd314afc090ff4cdf52886afe9b1b19333d147
Marlon Henrique <contato@marlonhenrique.com>
2019-09-09 13:49:01 -0300

    montando borda

--- boby/lib/main.dart
+++ boby/lib/main.dart
@@ -21,44 +21,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("BOBY", style: TextStyle(color: Colors.white, fontSize: 25.0), textAlign: TextAlign.center,),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text(
+          "BOBY",
+          style: TextStyle(color: Colors.black54, fontSize: 25.0),
+          textAlign: TextAlign.center,
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            height: 600.0,
-            padding: EdgeInsets.all(0),
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Container(
-                    color: Colors.amberAccent,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 600.0,
+          color: Colors.amber,
+          padding: EdgeInsets.all(0),
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                  onTap: () {
+                    print("Container clicked");
+                  },
+                  child: new Container(
+                    decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black12, style: BorderStyle.solid))),
+                    height: 70.0,
                     padding: EdgeInsets.all(10.0),
                     child: Column(
+
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
-                        Text(
-                          "Categoria",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 23.0,
-                          ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                "Categoria",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 23.0,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Icon(Icons.accessibility),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ),
-                );
-              },
-            ),
+              );
+            },
           ),
         ),
+      ),
     );
   }
 }
